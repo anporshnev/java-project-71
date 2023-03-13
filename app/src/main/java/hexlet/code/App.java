a39b5b26ed2d0773c2cf21a947266faa5ad48fba
AndrewPorshnev <andrew.porshnev@yandex.ru>
2023-03-13 15:38:48 +0300

    Добавляет аргументы и опции

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,9 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
 
 
 @Command(
@@ -11,6 +14,16 @@ import picocli.CommandLine.Command;
 )
 
 public class App {
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format;
+
     public static void main(String[] args) {
         var line = new CommandLine(new App()).execute(args);
         System.exit(line);
