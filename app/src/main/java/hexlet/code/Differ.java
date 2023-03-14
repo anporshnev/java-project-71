90bdf6b7d11e531d2aefcc3f5fc85c29e3db697d
AndrewPorshnev <andrew.porshnev@yandex.ru>
2023-03-14 10:57:05 +0300

    Добавляет класс Differ

---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public class Differ {
+    public static String generate(String filepath1, String filepath2) {
+        System.out.println(filepath1 + filepath2);
+        return "result string";
+    }
+}
