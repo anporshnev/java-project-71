a54684033675cc536f0d39fb8e35353bb0b3b52f
AndrewPorshnev <andrew.porshnev@yandex.ru>
2023-03-21 11:39:49 +0300

    Добавляет парсинг yaml

---
+++ app/src/main/java/hexlet/code/parsers/YamlParser.java
@@ -0,0 +1,16 @@
+package hexlet.code.parsers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.util.Map;
+
+public class YamlParser implements DataGenerator {
+    @Override
+    public Map<String, Object> parse(String content) throws JsonProcessingException {
+        ObjectMapper mapper = new YAMLMapper();
+        return mapper.readValue(content, new TypeReference<>() { });
+    }
+}
