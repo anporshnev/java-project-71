7794485e616f6426fbecfdb9b0f224a167f68888
AndrewPorshnev <andrew.porshnev@yandex.ru>
2023-03-21 15:34:05 +0300

    Добавляет класс объекта элемента разницы

---
+++ app/src/main/java/hexlet/code/DifferenceValue.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+public class DifferenceValue {
+    private final String status;
+    private final Object value1;
+    private Object value2;
+
+    public DifferenceValue(String status, Object value1) {
+        this.status = status;
+        this.value1 = value1;
+    }
+
+    public DifferenceValue(String status, Object value1, Object value2) {
+        this.status = status;
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public Object getValue2() {
+        return value2;
+    }
+
+    public Object getValue1() {
+        return value1;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
