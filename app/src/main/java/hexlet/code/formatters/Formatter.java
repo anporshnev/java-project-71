48e4fb4d4f9ff1e8661b07befd77b002e514ec51
AndrewPorshnev <andrew.porshnev@yandex.ru>
2023-03-23 14:47:43 +0300

    Добавляет формат plain в выбор форматов

--- app/src/main/java/hexlet/code/formatters/Formatter.java
+++ app/src/main/java/hexlet/code/formatters/Formatter.java
@@ -2,7 +2,10 @@ package hexlet.code.formatters;
 
 public class Formatter {
     public static FormatBuilder getFormatter(String format) {
-        return new StylishFormatter();
+        return switch (format) {
+            case "plain" -> new PlainFormatter();
+            default -> new StylishFormatter();
+        };
     }
 
 }
